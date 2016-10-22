9743119576bf912875be6915c67d8589f96f27ac
Craig Buchek <craig@boochtek.com>
2016-10-22 05:53:24 -0500

    Add Stays controller and views (index, new, edit)

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :shelters
+  resources :stays
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
