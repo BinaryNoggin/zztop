82535466cc1f7436432f6b879e82fcf63380b9d1
dummey <dummey@gmail.com>
2016-10-22 02:17:39 -0500

    Extracted transition logic out.

--- playground/replier.rb
+++ playground/replier.rb
@@ -2,86 +2,162 @@ require 'twilio-ruby'
 require 'sinatra'
 
 # fake db idea
-user = {id: 1, state: 1}
-state = {state: 1, response: 1}
+$valid_states = [:ask_housing, :ask_location]
 
-def get_state(user: 1)
-	{
-		1: {state: 1, response: 1}
-	}[user]
+$users = {
+    1 => {name: "bob"},
+    2 => {name: "sam"}
+}
+
+$states = {
+    1 => {state: :welcome, last_response: 1},
+    2 => {state: :ask_location, last_response: 2}
+}
+
+$transitions = {
+    #(current_state) => proc
+    :welcome => ->(user, input) {
+        set_state(user, :ask_housing, input)
+        return "Are you currently housed? (y or n)"
+    },
+    :ask_housing => ->(user, input) { 
+        case input
+        when "y", "Y", 1
+            set_state(user, :fin, input)
+            return "We are happy that you are currently housed."
+        when "n", "N", 0
+            set_state(user, :ask_location, input)
+            return "Provide us with a zip code if you want help. Reply with 'end' otherwise."
+        end
+    },
+    :ask_location => ->(user, input) {
+        case input
+        when "end"
+            set_state(user, :fin, input)
+            return "We are happy that you are currently housed."
+        when /([0-9]{5})/
+            set_state(user, :ask_shelter_perference, input)
+            #TODO: lookup shelters nearby
+            possible_shelters = find_shelters_for(user)
+
+            return "These are the options: " + possible_shelters.to_s
+        else
+            set_state(user, :ask_location, input)
+            return "Provide us with a zip code if you want help. Reply with 'end' otherwise."
+        end
+    },
+    :ask_shelter_perference => -> (user, input) {
+        valid_shelters = find_shelters_for(user).keys
+
+        if valid_shelters.contains?(input)
+            set_state(user, :fin, input)
+            return "You have a room booked at: " + find_shelters_for(user)[input]
+        else
+            return "These are the options: " + possible_shelters.to_s
+        end 
+    },
+    :thank_you => ->(user, input) {
+        return "Thank you for responding."
+    },
+    :emergency => ->(user, input) {
+
+    },
+    :fin => ->(user, input) {
+        return "Thank you for using our service."
+    }
+}
+
+### Shelter Lookup
+def find_shelters_for(user)
+    {
+        1 => {101 => {name: "Bob's Crab Shack", open_beds: 1}, 102 => {name: "Sam's Club", open_beds: 5}}
+    }[user]
 end
 
-def initiate_message
-	# put your own credentials here
-	account_sid = 'AC33bf897e53d130ee970e0682e0d434f6'
-	auth_token = 'e18c837ca2681e08b53fc7d928d7affa'
-
-	# set up a client to talk to the Twilio REST API
-	@client = Twilio::REST::Client.new account_sid, auth_token
-
-	# alternatively, you can preconfigure the client like so
-	Twilio.configure do |config|
-	  config.account_sid = account_sid
-	  config.auth_token = auth_token
-	end
-
-	# and then you can create a new client without parameters
-	@client = Twilio::REST::Client.new
-
-	#Send an SMS
-
-	@client.messages.create(
-	  from: '+15109013210',
-	  # to: '+15105906889',
-	  to: '+15732632278',
-	  body: 'Do you have shelter tonight? (0 or 1)'
-	)
+### State Functions
+def get_state(user)
+    $states[user]
 end
 
-def send_message(message)
+def clear_state(user)
+    $states[user] = {state: :welcome, response: 0}
+end
+
+def set_state(user, state, input)
+    $states[user] = {state: state, last_response: input}
+end
+
+def transition_state(user, from, input)
+    state = get_state(user)
+    puts "DEBUG: Transition on user: #{user}, with state #{state}"
+
+    transition = $transitions[from]
+    msg = transition.call(user, input)
+
+    state = get_state(user)
+    puts "DEBUG: Resulting user: #{user}, with state #{state}"
+    puts "DEBUG: Response: #{msg}"
+
+    return msg
+end
+
+### User Functions
+def get_user_id()
+    1
+end
+
+### Twilio Stuff
+def initiate_message(user_id)
+    # put your own credentials here
+    account_sid = 'AC33bf897e53d130ee970e0682e0d434f6'
+    auth_token = 'e18c837ca2681e08b53fc7d928d7affa'
+
+    # set up a client to talk to the Twilio REST API
+    @client = Twilio::REST::Client.new account_sid, auth_token
+
+    # alternatively, you can preconfigure the client like so
+    Twilio.configure do |config|
+      config.account_sid = account_sid
+      config.auth_token = auth_token
+    end
+
+    # and then you can create a new client without parameters
+    @client = Twilio::REST::Client.new
+
+    #reset and get first state, EDIT: acting weird because I am usinga global var
+    # clear_state(user_id);
+    # msg = transition_state(user_id, :welcome, 1)
+
+    #Send an SMS
+    @client.messages.create(
+      from: '+15109013210',
+      # to: '+15105906889',
+      to: '+15105906889',
+      body: "Welcome to the zztop less homeless system. Reply if you wish to continue."
+    )
+end
+
+def send_response(msg)
   twiml = Twilio::TwiML::Response.new do |r|
     r.Message msg
   end
   twiml.text
 end
 
-get '/test' do 
-	initiate_message()
+### Rest API 
+
+get '/initiate/:user_id' do 
+    initiate_message(params['user_id'])
 end
 
 post '/sms' do
-  puts "Message: #{params['Body']}!"
+  puts "DEBUG: Input #{params['Body']}!"
 
-  response = params['Body']
-  msg = ""
-  case get_state()[:state]
-  	when 0 
-  		#entry state
-	when 1
-		case response
-		when "0"
-			msg += "We are glad you are housed."
-		when "1"
-			msg += "Which zip code are you located at? We can help you find a shelter."
-			state[:state] = 2
-		end
-	when 2
-		#look up zip code area
-		shelter_list = [{name: "shelter 1"},{name: "shelter 2"}]
-		msg += "These are the shelters nearby: \n"
+  input = params['Body']
+  user = get_user_id()
+  state = get_state(user)
 
-		shelter_list.each_with_index do |shelter, i|
-			msg += "#{i}) #{shelter}\n"
-		end
-		state[:state] = 3
-	when 3
-		case response
-		when "0"
-			msg += "You have a bed at: 65491 Blvd.\n"
-		when "1"
-			msg += "You have a bed at: 12345 Street.\n"
-		end
-  end
+  msg = transition_state(user, state[:state], input)
 
-  send_message(msg)
+  send_response(msg)
 end
