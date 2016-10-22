eb6f851a5134330b5cc97ef54220d78b36cab8de
David Morse <dcmorse@gmail.com>
2016-10-22 17:23:34 -0500

    introduce History model, remove fields from user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,28 +2,21 @@
 #
 # Table name: users
 #
-#  id                    :integer          not null, primary key
-#  first_name            :string
-#  last_name             :string
-#  ssn                   :string
-#  dob                   :date
-#  created_at            :datetime         not null
-#  updated_at            :datetime         not null
-#  phone_number          :string
-#  fleeing               :boolean
-#  incarcerated          :boolean
-#  welfare               :boolean
-#  insufficent_income    :boolean
-#  alcohol_or_drug_abuse :boolean
-#  physical_health_issue :boolean
-#  mental_health_issue   :boolean
-#  exchange_for_sex      :boolean
-#  sex                   :string
-#  vet_status            :boolean
-#  grade                 :decimal(8, 5)
+#  id           :integer          not null, primary key
+#  first_name   :string
+#  last_name    :string
+#  ssn          :string
+#  dob          :date
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  phone_number :string
+#  sex          :string
+#  vet_status   :boolean
+#  grade        :decimal(8, 5)
 #
 
 class User < ApplicationRecord
+  has_many :histories
 
   def letter_grade
     case grade
---
+++ db/migrate/20161022221555_create_histories.rb
@@ -0,0 +1,19 @@
+class CreateHistories < ActiveRecord::Migration[5.0]
+  def change
+    create_table :histories do |t|
+      t.references :user, foreign_key: true
+      t.boolean :needs_shelter
+      t.integer :rating
+      t.boolean :fleeing
+      t.boolean :incarcerated
+      t.boolean :welfare
+      t.boolean :insufficent_income
+      t.boolean :alcohol_or_drug_abuse
+      t.boolean :physical_health_issue
+      t.boolean :mental_health_issue
+      t.boolean :exchange_for_sex
+
+      t.timestamps
+    end
+  end
+end
