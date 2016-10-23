9af48c1d2f08fc6c69bd415c405257aa77b45791
David Morse <dcmorse@gmail.com>
2016-10-22 23:09:53 -0500

    Display available shelters on user#show.
    CoC class knows all about all it's CoCs.

--- app/models/coc.rb
+++ app/models/coc.rb
@@ -1,15 +1,32 @@
 # Subclasses are written by a paid staff developer who understands the services offered by the CoC
 
 class Coc
-  def services(user)
-    raise "subclasses of CoC must implement #services method"
+  cattr_accessor :cocs
+  attr_reader :name, :filter
+
+  def initialize(name, &filter)
+    @name = name
+    @filter = filter
+    self.class.cocs ||= []
+    self.class.cocs << self
   end
-  def self.find_services(user)
-    [Biddle, CenterForWomenAndFamilies].map do |coc|
-      {
-        coc: coc,
-        services: coc.new.services(user)
-      }
-    end
+
+  def self.find_for_user(user)
+    cocs.map {|coc| {name: coc.name, services: coc.filter.call(user)}}.select {|hash| hash[:services]}
   end
 end
+
+Biddle = Coc.new('Biddle House') do |user|
+  [:meal, :housing] if user.ssn.present?
+end
+CFWF = Coc.new('Center for Women and Families') do |user|
+  [:housing] if user.dob + 16.years >= Date.today || !user.male?
+end
+
+
+# Booked = Coc.new('Always Booked') do |user|
+#   []
+# end
+# Food = Coc.new('Always Food') do |user|
+#   [:meal]
+# end
