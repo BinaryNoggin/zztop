ad588f3fdfb1c9db15b1b1cb239b40ef50d1fb79
Amos L King <amos.l.king@gmail.com>
2016-10-22 21:47:39 -0500

    Collects user input from phone call
    Amos King @adkron <amos@binarynoggin.com>

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -5,12 +5,27 @@ class TwilioController < ApplicationController
 
   skip_before_action :verify_authenticity_token
 
+  def respond_to_homeless_voice
+    response = params[:Digits].to_i
+    Twilio::TwiML::Response.new do |r|
+      r.Say "Thanks for responding with #{response}, #{user.first_name}", :voice => 'alice'
+    end
+
+
+    render_twiml response
+  end
+
   def voice
-    user = User.find(params[:user_id])
-    response = Twilio::TwiML::Response.new do |r|
-      r.Say "Hey there, #{user.first_name}.", :voice => 'alice'
+    Twilio::TwiML::Response.new do |r|
+      r.Gather numDigits: 1, action: twilio_respond_to_homeless_voice_url(user) do |g|
+        g.Say "Hey there, #{user.first_name}. Do you have a place to stay tonight? Press one for yes or two for no.", :voice => 'alice'
+      end
     end
 
     render_twiml response
+  end
+
+  def user(User)
+    @user ||= User.find(params[:user_id])
   end
 end
