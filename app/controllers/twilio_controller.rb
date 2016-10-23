9088b626d5ecf30e7063c5733586002c6119e835
Amos L King <amos.l.king@gmail.com>
2016-10-22 22:00:31 -0500

    Updates response for twilio
    Amos King @adkron <amos@binarynoggin.com>
    Roger Pincombe @okgodoit <github@betechie.com>

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -6,9 +6,9 @@ class TwilioController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def respond_to_homeless_voice
-    response = params[:Digits].to_i
-    Twilio::TwiML::Response.new do |r|
-      r.Say "Thanks for responding with #{response}, #{user.first_name}", :voice => 'alice'
+    answer = params[:Digits].to_i
+    response = Twilio::TwiML::Response.new do |r|
+      r.Say "Thanks for responding with #{answer}, #{user.first_name}", :voice => 'alice'
     end
 
 
@@ -16,7 +16,7 @@ class TwilioController < ApplicationController
   end
 
   def voice
-    Twilio::TwiML::Response.new do |r|
+    response = Twilio::TwiML::Response.new do |r|
       r.Gather numDigits: 1, action: user_twilio_respond_to_homeless_voice_url(user) do |g|
         g.Say "Hey there, #{user.first_name}. Do you have a place to stay tonight? Press one for yes or two for no.", :voice => 'alice'
       end
