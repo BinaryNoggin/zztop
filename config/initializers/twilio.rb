ad9e10642cd0a440c55589879e3df07ae574e9dd
Amos L King <amos.l.king@gmail.com>
2016-10-22 21:21:45 -0500

    Updates phome number
    314.310.0bed
    
    Amos King @adkron <amos@binarynoggin.com>

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,4 +1,4 @@
-PHONE_NUMBER = "+13142070404"
+PHONE_NUMBER = "+13143100233"
 Twilio.configure do |config|
   config.account_sid = Rails.application.secrets.TWILIO_ACCOUNT_SID
   config.auth_token = Rails.application.secrets.TWILIO_AUTH_TOKEN
